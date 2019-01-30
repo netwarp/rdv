5ad6ff53ea5abfc3ab696674c9f1640d28dd2ac2
Unknown <unknown@example.com>
2019-01-30 21:04:15 +0100

    tables users + ads + messages + posts

---
+++ app/Http/Controllers/Front/FrontController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FrontController extends Controller
+{
+    public function getIndex() {
+        return view('front.index');
+    }
+
+
+}
---
+++ database/migrations/2019_01_30_185119_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image');
+            $table->string('tags');
+            $table->text('preview');
+            $table->text('content');
+            $table->integer('nb_comments')->default(0);
+            $table->enum('status', ['draft', 'published', 'unpublished'])->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
