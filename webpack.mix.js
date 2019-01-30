a5145cd4a5f3abff796e09e0954a55451168fa32
Unknown <unknown@example.com>
2019-01-30 21:05:57 +0100

    add images

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+mix.options({
+    processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+});
 mix
     .js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
