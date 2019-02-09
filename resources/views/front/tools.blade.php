6d812cbf3d5a122cbd3a82a44139f57dc6da6e95
Unknown <unknown@example.com>
2019-02-09 20:07:48 +0100

    commit 0 tools

--- resources/views/front/tools.blade.php
+++ resources/views/front/tools.blade.php
@@ -5,6 +5,9 @@
         <div class="row">
             <div class="col-md-9">
                 <h1 class="h3">Outils</h1>
+                <div class="alert alert-info">
+                    Traduction de <a href="https://www.privacytools.io/#os">privacytools.io</a>
+                </div>
             </div>
         </div>
     </div>
