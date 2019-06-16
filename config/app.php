8cedded2028491075c874eb0bbaf64dc5ec2864c
netwarp <resethread@gmail.com>
2019-06-16 22:22:24 +0200

    start mongodb

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Debugbar\ServiceProvider::class,
-
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
     ],
 
     /*
